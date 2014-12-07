770aceb80413985036393e3791417185f47e2c2a
Francois-Xavier Aeberhard <fx@red-agent.com>
2014-12-07 14:33:05 +0100

    First try

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	//private Animator animator;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")){
+			Animator animator =  this.GetComponent<Animator>();
+			animator.Play("SwitchScreen");
+			print ("space key was pressed");
+		}
+	}
+}
